5d4b3dbf79bc16bd425f3c741460a271f32a3092
Ashwin2173 <87991090+Ashwin2173@users.noreply.github.com>
2026-08-06 23:05:54 +0530

    Added len() native function

--- include/list.h
+++ include/list.h
@@ -9,8 +9,6 @@ typedef struct LM_List {
     size_t capacity;
 } LM_List;
 
-// void list_init();
-// void list_empty_init(LM_List *list);
 // void list_append(LM_List *list, LM_Value value);
 // void list_gc(LM_List *list);
 
--- src/native_functions.c
+++ src/native_functions.c
@@ -4,6 +4,7 @@
 #include <inttypes.h>
 #include <windows.h>
 
+#include "../include/error.h"
 #include "../include/list.h"
 #include "../include/value.h"
 
@@ -81,4 +82,17 @@ LM_Value native_now(const size_t argc, LM_Value* argv) {
     QueryPerformanceCounter(&counter);
     const int64_t us = (counter.QuadPart * 1000000LL) / freq.QuadPart;
     return make_int(us);
+}
+
+LM_Value native_len(const size_t argc, LM_Value* argv) {
+    if (argc == 1) {
+        if (argv[0].type == LM_LIST) {
+            return make_int((int64_t) argv[0].as.list->length);
+        }
+        if (argv[0].type == LM_STRING) {
+            return make_int((int64_t) argv[0].as.string->length);
+        }
+        Fault(TYPE_ERROR, "Invalid type for len()");
+    }
+    return make_int(0);
 }
